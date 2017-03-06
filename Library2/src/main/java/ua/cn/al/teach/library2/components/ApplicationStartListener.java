602e213e74613833693734e145ffc69d258d9c4e
Oleksiy Lukin <alukin@gmail.com>
2017-03-06 12:18:35 +0200

    Application start listener added

---
+++ Library2/src/main/java/ua/cn/al/teach/library2/components/ApplicationStartListener.java
@@ -0,0 +1,24 @@
+
+package ua.cn.al.teach.library2.components;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author al
+ */
+@Component
+public class ApplicationStartListener implements ApplicationListener<ContextRefreshedEvent>{
+private static final Logger logger =  LoggerFactory.getLogger(ApplicationStartListener.class);
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+       logger.warn("======================== Application prepared event fired! ===========================");
+    }
+    
+}
