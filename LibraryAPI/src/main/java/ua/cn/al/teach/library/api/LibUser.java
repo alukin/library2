3408f263a0181dc9b31a8c1fabc7d430df473ada
Oleksiy Lukin <alukin@gmail.com>
2017-03-27 16:27:46 +0300

    User Id changed to string because Long could be longer that IEEE double

--- LibraryAPI/src/main/java/ua/cn/al/teach/library/api/LibUser.java
+++ LibraryAPI/src/main/java/ua/cn/al/teach/library/api/LibUser.java
@@ -14,7 +14,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class LibUser {
     @XmlElement(required=false)
-    public Long user_id;
+    public String user_id;
     @XmlElement(required=true)
     public String login;
     @XmlElement(required=true)
